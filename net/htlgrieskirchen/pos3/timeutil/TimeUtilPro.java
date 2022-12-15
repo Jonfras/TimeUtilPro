9135db2978ad35f0e3a21a89f1e221a8e256e79c
Jonfras <jonas.kreuzh@gmail.com>
2022-12-15 23:02:53 +0100

    finished(ich hasse date)

--- net/htlgrieskirchen/pos3/timeutil/TimeUtilPro.java
+++ net/htlgrieskirchen/pos3/timeutil/TimeUtilPro.java
@@ -8,14 +8,21 @@ import java.util.Date;
 
 
 public class TimeUtilPro {
+
     private TimeUtilPro() {
     }
 
     // ########## LOCALDATE METHODS ##########
 
+    /**
+     * Länge vom jahr is zurzeit 4 stellig kunnt ma theoretisch oba a fünfstellig mocha war oba aufwand
+     * und is ned in da angabe gstondn
+     */
+
     public static LocalDate intToLocalDate(int date) {
         LocalDate localDate;
         String stringDate = String.valueOf(date);
+
         if (stringDate.length() == 8) {
             try {
                 localDate = LocalDate.of(
@@ -26,8 +33,7 @@ public class TimeUtilPro {
                 log("intToLocalDate", "invalid digit");
                 return null;
             }
-        }
-        else {
+        } else {
             log("intToLocalDate", "invalid Integer length");
             return null;
         }
@@ -54,13 +60,12 @@ public class TimeUtilPro {
                 localDate = LocalDate.of(
                         Integer.parseInt(stringDate.substring(0, 4)),
                         Integer.parseInt(stringDate.substring(4, 6)),
-                        Integer.parseInt(stringDate.substring(6,8)));
+                        Integer.parseInt(stringDate.substring(6, 8)));
             } catch (DateTimeException e) {
                 log("longToLocalDate", "invalid digit");
                 return null;
             }
-        }
-        else {
+        } else {
             log("longToLocalDate", "invalid Long length");
             return null;
         }
@@ -73,11 +78,10 @@ public class TimeUtilPro {
         try {
             localDate = LocalDate.of(
                     dateTime.getYear(),
-                    dateTime.getMonth()+1,
-                    dateTime.getDate());
-
-        }
-        catch (NullPointerException e){
+                    dateTime.getMonth(),
+                    dateTime.getDate()
+            );
+        } catch (NullPointerException e) {
             log("dateToLocalDate", "the given Date object was null");
             return null;
         }
@@ -89,11 +93,10 @@ public class TimeUtilPro {
         try {
             localDate = LocalDate.of(
                     dateTime.get(Calendar.YEAR),
-                    dateTime.get(Calendar.MONTH)+1,
+                    dateTime.get(Calendar.MONTH),
                     dateTime.get(Calendar.DAY_OF_MONTH));
-            
-        }
-        catch (NullPointerException e){
+
+        } catch (NullPointerException e) {
             log("calendarToLocalDate", "the given Calendar object was null");
             return null;
         }
@@ -104,65 +107,170 @@ public class TimeUtilPro {
     // ########## LOCALDATETIME METHODS ##########
 
     public static LocalDateTime intToLocalDateTime(int date) {
-        return null;
+        LocalDateTime localDateTime;
+        String stringDate = String.valueOf(date);
+        if (stringDate.length() == 8) {
+            try {
+                localDateTime = LocalDateTime.of(
+                        Integer.parseInt(stringDate.substring(0, 4)),
+                        Integer.parseInt(stringDate.substring(4, 6)),
+                        Integer.parseInt(stringDate.substring(6)),
+                        0,
+                        0
+                );
+            } catch (DateTimeException e) {
+                log("intToLocalDateTime", "invalid digit");
+                return null;
+            }
+        } else {
+            log("intToLocalDateTime", "invalid Integer length");
+            return null;
+        }
+        return localDateTime;
     }
 
     public static LocalDateTime longToLocalDateTime(long dateTime) {
-        return null;
+        LocalDateTime localDateTime;
+        String stringDate = String.valueOf(dateTime);
+        if (stringDate.length() == 12) {
+            try {
+                localDateTime = LocalDateTime.of(
+                        Integer.parseInt(stringDate.substring(0, 4)),
+                        Integer.parseInt(stringDate.substring(4, 6)),
+                        Integer.parseInt(stringDate.substring(6, 8)),
+                        Integer.parseInt(stringDate.substring(8, 10)),
+                        Integer.parseInt(stringDate.substring(10))
+                );
+            } catch (DateTimeException e) {
+                log("longToLocalDateTime", "invalid digit");
+                return null;
+            }
+        } else {
+            log("longToLocalDateTime", "invalid Long length");
+            return null;
+        }
+        return localDateTime;
     }
 
     public static LocalDateTime dateToLocalDateTime(Date dateTime) {
-        return null;
+        LocalDateTime localDateTime;
+
+        try {
+            localDateTime = LocalDateTime.of(
+                    dateTime.getYear(),
+                    dateTime.getMonth(),
+                    dateTime.getDate(),
+                    dateTime.getHours(),
+                    dateTime.getMinutes()
+            );
+        } catch (NullPointerException e) {
+            log("dateToLocalDateTime", "the given Date object was null");
+            return null;
+        }
+        return localDateTime;
     }
 
     public static LocalDateTime calendarToLocalDateTime(Calendar dateTime) {
-        return null;
+        LocalDateTime localDateTime;
+        System.out.println();
+        try {
+            localDateTime = LocalDateTime.of(
+                    dateTime.get(Calendar.YEAR),
+                    dateTime.get(Calendar.MONTH),
+                    dateTime.get(Calendar.DAY_OF_MONTH),
+                    dateTime.get(Calendar.HOUR_OF_DAY),
+                    dateTime.get(Calendar.MINUTE)
+            );
+        } catch (NullPointerException e) {
+            log("calendarToLocalDateTime", "the given Calendar object was null");
+            return null;
+        }
+
+        return localDateTime;
     }
 
     // ########## INT METHODS ##########
 
     public static int localDateToInt(LocalDate date) {
-        return -1;
+        return (int) convertToNumber(date, 4);
     }
 
+
     public static int localDateTimeToInt(LocalDateTime dateTime) {
-        return -1;
+        return (int) convertToNumber(dateTime.toLocalDate(), 4);
     }
 
     // ########## LONG METHODS ##########
 
     public static long localDateToLong(LocalDate date) {
-        return -1L;
+
+        return convertToNumber(date, 9);
     }
 
     public static long localDateTimeToLong(LocalDateTime dateTime) {
-        return -1L;
+        long date = convertToNumber(dateTime.toLocalDate(), 9) * 10000;
+        long time = dateTime.getHour() * 100 + dateTime.getMinute();
+        return date + time;
+
     }
 
     // ########## DATE METHODS ##########
 
     @SuppressWarnings("deprecation")
     public static Date localDateToDate(LocalDate date) {
-        return null;
+        return new Date(date.getYear(), date.getMonthValue()-1, date.getDayOfMonth());
     }
 
     @SuppressWarnings("deprecation")
     public static Date localDateTimeToDate(LocalDateTime dateTime) {
-        return null;
+        Date date = localDateToDate(dateTime.toLocalDate());
+        date.setHours(dateTime.getHour());
+        date.setMinutes(dateTime.getMinute());
+        return date;
     }
 
     // ########## CALENDAR METHODS ##########
 
     public static Calendar localDateToCalendar(LocalDate date) {
-        return null;
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(date.getYear(), date.getMonthValue()-1, date.getDayOfMonth());
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar;
     }
 
     public static Calendar localDateTimeToCalendar(LocalDateTime dateTime) {
-        return null;
+        Calendar calendar = localDateToCalendar(dateTime.toLocalDate());
+        calendar.set(Calendar.HOUR_OF_DAY, dateTime.getHour());
+        calendar.set(Calendar.MINUTE, dateTime.getMinute());
+        return calendar;
     }
+
+    private static long convertToNumber(LocalDate date, int maxYearLength) {
+
+        StringBuilder year = new StringBuilder(String.valueOf(date.getYear()));
+
+        if (year.toString().length() > maxYearLength) { // change this to five if you want 5 digits for the year
+            log("localDateToInt", "year is to long");
+            return -1;
+        }
+
+        StringBuilder month = new StringBuilder(String.valueOf(date.getMonthValue()));
+        if (month.toString().length() < 2) {
+            month.insert(0, 0);
+        }
+
+        StringBuilder day = new StringBuilder(String.valueOf(date.getDayOfMonth()));
+        if (day.toString().length() < 2) {
+            day.insert(0, 0);
+        }
+
+        String longValue = year.toString() + month + day;
+
+        return Long.parseLong(longValue);
+    }
+
 
     public static void log(String methodName, String message) {
         System.err.println("!ERROR AT: " + methodName + ": " + message);
     }
-
 }
--- net/htlgrieskirchen/pos3/timeutil/TimeUtilProTest.java
+++ net/htlgrieskirchen/pos3/timeutil/TimeUtilProTest.java
@@ -3,6 +3,7 @@ package net.htlgrieskirchen.pos3.timeutil;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Calendar;
@@ -39,7 +40,11 @@ public class TimeUtilProTest {
     public void dateToLocalDate() {
         LocalDate expected = LocalDate.of(2003, 2, 1);
 
-        Date date = new Date(2003, Calendar.FEBRUARY, 1);
+        //wenns mid an int bei month ned bast don muast in TimeUtilPro
+        // bei Month einfoch wieder des +1 dazua doa
+        // des söbe bei de mid calendar
+
+        Date date = new Date(2003, 2, 1);
         LocalDate actual = TimeUtilPro.dateToLocalDate(date);
 
 
@@ -51,7 +56,7 @@ public class TimeUtilProTest {
         LocalDate expected = LocalDate.of(2003,2,1);
 
         Calendar calendar = Calendar.getInstance();
-        calendar.set(2003, Calendar.FEBRUARY, 1);
+        calendar.set(2003, 2, 1);
         LocalDate actual = TimeUtilPro.calendarToLocalDate(calendar);
 
         assertEquals(expected, actual);
@@ -80,9 +85,11 @@ public class TimeUtilProTest {
     @Test
     public void dateToLocalDateTime() {
         LocalDateTime expected = LocalDateTime.of(
-                2003, 2, 1, 0,0);
+                2003, 2, 1, 12,34);
 
-        Date date = new Date(2003, Calendar.FEBRUARY, 1);
+        Date date = new Date(2003, 2, 1);
+        date.setHours(12);
+        date.setMinutes(34);
         LocalDateTime actual = TimeUtilPro.dateToLocalDateTime(date);
 
 
@@ -92,5 +99,101 @@ public class TimeUtilProTest {
 
     @Test
     public void calendarToLocalDateTime() {
+        LocalDateTime expected = LocalDateTime.of(
+                2003, 2, 1, 12,34);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2003, 2, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 12);
+        calendar.set(Calendar.MINUTE, 34);
+
+        LocalDateTime actual = TimeUtilPro.calendarToLocalDateTime(calendar);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void localDateToInt() {
+        int expected = 20030201;
+
+        LocalDate localDate = LocalDate.of(2003, 2,1);
+
+        int actual = TimeUtilPro.localDateToInt(localDate);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void localDateTimeToInt() {
+        int expected = 20030201;
+
+        LocalDateTime localDateTime = LocalDateTime.of(2003, 2, 1, 12, 34);
+
+        int actual = TimeUtilPro.localDateTimeToInt(localDateTime);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void localDateToLong() {
+        long expected = 9999999991231L;
+
+        LocalDate localDate = LocalDate.of(999999999,12,31);
+
+        long actual = TimeUtilPro.localDateToLong(localDate);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void localDateTimeToLong() {
+        long expected = 200312311234L;
+
+        LocalDateTime localDateTime = LocalDateTime.of(2003,12,31,12,34);
+
+        long actual = TimeUtilPro.localDateTimeToLong(localDateTime);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void localDateToDate() {
+        Date expected = new Date(2003, Calendar.FEBRUARY, 1);
+
+        Date actual = TimeUtilPro.localDateToDate(LocalDate.of(2003, 2,1));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void localDateTimeToDate() {
+        Date expected = new Date(2003, Calendar.FEBRUARY, 1, 12, 34);
+
+        Date actual = TimeUtilPro.localDateTimeToDate(LocalDateTime.of(2003, 2,1,12,34));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void localDateToCalendar() {
+        Calendar expected = Calendar.getInstance();
+        expected.set(2003, Calendar.FEBRUARY, 1);
+        expected.set(Calendar.MILLISECOND, 0);
+
+        Calendar actual = TimeUtilPro.localDateToCalendar(LocalDate.of(2003, 2,1));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void localDateTimeToCalendar() {
+        Calendar expected = Calendar.getInstance();
+        expected.set(2003, Calendar.FEBRUARY, 1);
+        expected.set(Calendar.HOUR_OF_DAY, 12);
+        expected.set(Calendar.MINUTE, 34);
+
+        Calendar actual = TimeUtilPro.localDateTimeToCalendar(LocalDateTime.of(2003, 2,1, 12, 34));
+
+        assertEquals(expected, actual);
     }
 }
